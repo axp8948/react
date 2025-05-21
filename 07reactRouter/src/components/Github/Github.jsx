db2aa8b3cb4d4ca87dfa0fed83ab9177da68c17f
Anmol Pandey <axp8948@mavs.uta.edu>
2025-05-21 13:23:13 -0500

    Changes in router

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,16 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 function Github() {
-    useEffect(() => {
-        fetch()
-    }, [])
 
-    // need to use useEffect to make sure 
-    return (
-        <div className="text-center m-4 bg-gray-500 text-white text-3xl p-4">
-            Github followers: 
-        </div>
-    )
+    const data = useLoaderData();
+
+    // const [data, setData] = useState([])
+
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/axp8948')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // }, [])
+
+    // need to use useEffect to make sure fetch is executed only after the component is loaded
+return (
+  <div className="max-w-md mx-auto my-8 bg-gray-600 text-white rounded-xl shadow-lg p-6 text-center space-y-6">
+    <h2 className="text-3xl font-bold text-white">GitHub Followers</h2>
+    <p className="text-4xl font-semibold text-white">{data.followers}</p>
+
+    <div className="flex flex-col items-center">
+      <p className="mb-2 text-lg text-white">GitHub Avatar</p>
+      <div className="overflow-hidden rounded-full shadow-md transition-transform duration-300 hover:scale-105 w-32 h-32 border-4 ">
+        <img
+          src={data.avatar_url}
+          alt="GitHub Avatar"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+
 }
 
 export default Github
+
+export const githubInfoLoader = async() => {
+    const response = await fetch('https://api.github.com/users/axp8948')
+    return response.json()
+}
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -8,7 +8,7 @@ import About from './components/About/About.jsx'
 import Home from './components/Home/Home.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
-import Github from './components/Github/Github.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -39,8 +39,10 @@ const router = createBrowserRouter(
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
-      <Route path='github' element={<Github />} />
-
+      <Route
+        path='github'
+        element={<Github />} 
+        loader={githubInfoLoader} />
     </Route>
   )
 )
